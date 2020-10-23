ada16df61108755d8e38b331ef6276c74263df11
Hirotaka Ishihara <38371297+JerryIshihara@users.noreply.github.com>
2020-10-22 23:17:58 -0400

    Added service.yml & sentry (#5)
    * added bootstrap
    
    * Update action.yml
    
    * Update action.yml
    
    * update bootstrap
    
    * fix bootstrap to sh syntax
    
    * fix bootstrap to sh syntax
    
    * update bootstrap
    
    * use pip3
    
    * setup-tool
    
    * py to python3
    
    * remove py app.y
    
    * pytest
    
    * add service.yml
    
    * add sentry to requirements.txt
    
    * add import sentry_sdk

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://77efe2bf05f142aaa7383d2764717b05@o465899.ingest.sentry.io/5479436",
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0
+)
 
 app = Flask(__name__)
 CORS(app)
