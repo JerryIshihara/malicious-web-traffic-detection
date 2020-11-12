81965215f9f407762d87a18ab26702c09ac9194a
Hirotaka Ishihara <hirotaka.ishihara@mail.utoronto.ca>
2020-11-12 18:44:21 -0500

    added baseline model

--- test/test_baseline.py
+++ test/test_baseline.py
@@ -1,10 +1,62 @@
 import pytest
 import tempfile
 import numpy as np
+import json
 import os
 import sys
 sys.path.append('../model')
 import model
+
+benign_data = {
+	"SourceIP": "0.0.0.0", 
+	"DestinationIP": "0.0.0.0", 
+	"SourcePort": 12, 
+	"DestinationPort": 24, 
+	"TimeStamp": "21231", 
+	"Duration": 12.4124, 
+	"FlowBytesSent": 125, 
+	"FlowSentRate": 8692.12, 
+    "FlowBytesReceived": 515, 
+    "FlowReceivedRate": 515.523,
+    "PacketLengthMean": 515.523, 
+    "PacketTimeMean": 515.523, 
+    "ResponseTimeTimeMean": 515.523, 
+    "DoH": True
+}
+
+mal_data = {
+	"SourceIP": "192.168.20.212", 
+	"DestinationIP": "9.9.9.11", 
+	"SourcePort": 54066, 
+	"DestinationPort": 443, 
+	"TimeStamp": "2020-03-31 21:24:25", 
+	"Duration": 34.0556, 
+	"FlowBytesSent": 1875, 
+	"FlowSentRate": 55.0571, 
+    "FlowBytesReceived": 4964, 
+    "FlowReceivedRate": 145.762,
+    "PacketLengthMean": 213.719, 
+    "PacketTimeMean": 7.86213, 
+    "ResponseTimeTimeMean": 0.0098586, 
+    "DoH": True
+}
+
+wrong_data = {
+	"SourceIP": 192, 
+	"DestinationIP": "9.9.9.11", 
+	"SourcePort": 54066, 
+	"DestinationPort": 443, 
+	"TimeStamp": "2020-03-31 21:24:25", 
+	"Duration": 34.0556, 
+	"FlowBytesSent": 1875, 
+	"FlowSentRate": 55.0571, 
+    "FlowBytesReceived": 4964, 
+    "FlowReceivedRate": 145.762,
+    "PacketLengthMean": 213.719, 
+    "PacketTimeMean": 7.86213, 
+    "ResponseTimeTimeMean": 0.0098586, 
+    "DoH": True
+}
 
 
 @pytest.fixture
@@ -25,13 +77,22 @@ def test_forward(client):
     Args:
         client ([type: class]): [description: Flask test client]
     """
-    # generate random int
-    for state in np.random.randint(999, size=10):
-        URL = f'/predict/{state}'
-        response = client.get(URL)
-        assert response.status_code == 200
-        assert response.get_data().decode('utf-8') == str(state)
-    # should fail on string input
-    URL = '/predict/string'
-    response = client.get(URL)
-    assert response.status_code == 404
+    # with correct input format
+    URL = '/predict'
+    TYPE = 'application/json'
+    response = client.post(URL, 
+                           data=json.dumps(benign_data),
+                           content_type=TYPE)
+    assert response.status_code == 200
+    response = client.post(URL, 
+                           data=json.dumps(mal_data),
+                           content_type=TYPE)
+    assert response.status_code == 200
+    # with wrong data type
+    response = client.post(URL, 
+                        data=json.dumps(wrong_data),
+                        content_type=TYPE)
+    assert response.status_code == 403
+    # with no input
+    response = client.post(URL)
+    assert response.status_code == 403
