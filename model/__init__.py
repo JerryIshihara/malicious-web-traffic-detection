61d4a03adee4d02ad2387bf9fc749e6ea426c05f
Hirotaka Ishihara <hirotaka.ishihara@mail.utoronto.ca>
2020-11-27 07:28:44 -0500

    put back sentry

--- model/__init__.py
+++ model/__init__.py
@@ -3,11 +3,11 @@ from flask_cors import CORS
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-# sentry_sdk.init(
-#     dsn="https://f7bf6da58d384c7cacf967b90540b879@o358880.ingest.sentry.io/5480579",
-#     integrations=[FlaskIntegration()],
-#     traces_sample_rate=1.0
-# )
+sentry_sdk.init(
+    dsn="https://f7bf6da58d384c7cacf967b90540b879@o358880.ingest.sentry.io/5480579",
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0
+)
 
 app = Flask(__name__)
 CORS(app)
