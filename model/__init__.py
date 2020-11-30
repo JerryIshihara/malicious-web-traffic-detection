6a8a9f2543714e44a52a66aed10e6440b6b3c564
Hirotaka Ishihara <hirotaka.ishihara@mail.utoronto.ca>
2020-11-29 22:14:26 -0500

    test coverage

--- model/__init__.py
+++ model/__init__.py
@@ -6,7 +6,7 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 sentry_sdk.init(
     dsn="https://f7bf6da58d384c7cacf967b90540b879@o358880.ingest.sentry.io/5480579",
     integrations=[FlaskIntegration()],
-    traces_sample_rate=1.0
+    traces_sample_rate=10.0
 )
 
 app = Flask(__name__)
--- model/utils.py
+++ model/utils.py
@@ -11,30 +11,9 @@ def check_data_format(X):
         assert type(X['FlowSentRate']) == float
         assert type(X['FlowBytesReceived']) == int
         assert type(X['FlowReceivedRate']) == float
-        # assert type(X['PacketLengthVariance']) == float
-        # assert type(X['PacketLengthStandardDeviation']) == float
         assert type(X['PacketLengthMean']) == float
-        # assert type(X['PacketLengthMedian']) == float
-        # assert type(X['PacketLengthMode']) == float
-        # assert type(X['PacketLengthSkewFromMedian']) == float
-        # assert type(X['PacketLengthSkewFromMode']) == float
-        # assert type(X['PacketLengthCoefficientofVariation']) == float
-        # assert type(X['PacketTimeVariance']) == float
-        # assert type(X['PacketTimeStandardDeviation']) == float
         assert type(X['PacketTimeMean']) == float
-        # assert type(X['PacketTimeMedian']) == float
-        # assert type(X['PacketTimeMode']) == float
-        # assert type(X['PacketTimeSkewFromMedian']) == float
-        # assert type(X['PacketTimeSkewFromMode']) == float
-        # assert type(X['PacketTimeCoefficientofVariation ']) == float
-        # assert type(X['ResponseTimeTimeVariance']) == float
-        # assert type(X['ResponseTimeTimeStandardDeviation']) == float
         assert type(X['ResponseTimeTimeMean']) == float
-        # assert type(X['ResponseTimeTimeMedian']) == float
-        # assert type(X['ResponseTimeTimeMode']) == float
-        # assert type(X['ResponseTimeTimeSkewFromMedian']) == float
-        # assert type(X['ResponseTimeTimeSkewFromMode']) == float
-        # assert type(X['ResponseTimeTimeCoefficientofVariation ']) == float
         assert type(X['DoH']) == bool
     except: return False
     return True
